a3ceda2e1cef92ea1847597282e563de5228db27
Shraddha Gupta <75632406+shradhagupta12@users.noreply.github.com>
2024-06-10 21:55:16 +0530

    V1

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-
-const hamburgerIcon = document.getElementById('hamburger-icon');
-const menu = document.querySelector('.menu-item');
-
 document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerIcon = document.getElementById('hamburger-icon');
+    const menu = document.querySelector('.menu-item');
+    const menuItems = document.querySelectorAll('.menu-item li a');
+
     hamburgerIcon.addEventListener('click', function () {
         menu.classList.toggle('show');
     });
 
-    // Hide the menu initially on smaller screens
-    if (window.innerWidth <= 768) {
-        menu.classList.remove('show');
-    }
+    // Add click event listeners to each list item
+    menuItems.forEach(item => {
+        item.addEventListener('click', function () {
+            // Hide the menu when a list item is clicked
+            menu.classList.remove('show'); 
+        });
+    });
+    
 });
